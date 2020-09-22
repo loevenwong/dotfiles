c6316974545f61f964be6ceaf55a8ffedf9483f2
Timo Lövenich <timo.loevenich@aoe.com>
2020-09-22 10:42:56 +0200

    changes

--- shell/env.sh
+++ shell/env.sh
@@ -1,17 +1,16 @@
 export EDITOR="subl"
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 export HIST_STAMPS="dd.mm.yy"
-export DEFAULT_USER=t.loevenich
+export DEFAULT_USER=timo
 
 # homebrew and bin
 export PATH="/usr/local/sbin:/usr/local/bin:$PATH"
 # use coreutils instead of osx ones
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 # use home .bin folder
-export PATH="/Users/t.loevenich/.bin:$PATH"
+export PATH="~/.bin:~/bin:$PATH"
 
 # other path settings...
-export PATH=$PATH:/Users/t.loevenich/private/dev/android-sdk-macosx/platform-tools
 export POWERLEVEL9K_MODE='awesome-patched'
 
 #docker_env
--- zshrc
+++ zshrc
@@ -142,3 +142,12 @@ fi
 CASE_SENSITIVE="true"
 zstyle ':completion:*' special-dirs true
 zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(
+  git
+  docker
+)
